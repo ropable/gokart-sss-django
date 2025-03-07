aa5d90badfb224d417272ae6c68e4a94933de89e
Nishant Phour <phournishant0@gmail.com>
2025-03-07 14:19:20 +0800

    Kmi requests wrapped around proxy

--- sss/context_processors.py
+++ sss/context_processors.py
@@ -4,19 +4,21 @@
 # Third-Party
 from django import conf
 from django import http
+from sss.models import MapServer
 
 # Typing
 from typing import Any
 
 
 def variables(request):
-
+    kmi_server = MapServer.objects.get(name='kmi').url
+    base_url = f"{request.scheme}://{request.get_host()}"
     # Construct and return context  
     return {
 #        "app_build_url": conf.settings.DEV_APP_BUILD_URL,
         "GIT_COMMIT_HASH": conf.settings.GIT_COMMIT_HASH,
         "DJANGO_SETTINGS": conf.settings,
         "settings": conf.settings,
-        "kmi_url": conf.settings.KMI_URL
+        "kmi_url": base_url + kmi_server
 
     }
